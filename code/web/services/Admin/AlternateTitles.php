a7834ed686866dab61548ad1643b9dbfd1bf2e3b
Yanjun Li <74245023+LiYanjun19@users.noreply.github.com>
2025-05-30 12:14:10 -0400

    DIS-906 Fix the error of Added By filter in Manual Grouping Authorities

--- code/web/services/Admin/AlternateTitles.php
+++ code/web/services/Admin/AlternateTitles.php
@@ -68,4 +68,42 @@ class Admin_AlternateTitles extends ObjectEditor {
 	function canAddNew() {
 		return false;
 	}
+
+	function applyFilter(DataObject $object, string $fieldName, array $filter) {
+		if ($fieldName == 'addedByName') {
+			$matchingUserIds = [];
+
+			if (($filter['filterType'] == 'matches' && $filter['filterValue'] == '')) {
+				$object->whereAdd("addedBy IS NULL");
+				return;
+			}
+			$user = new User();
+			$user->whereAdd('id IN (SELECT DISTINCT addedBy FROM grouped_work_alternate_titles WHERE addedBy IS NOT NULL)');
+			$user->find();
+			while ($user->fetch()) {
+				$userDisplayName = $user->getDisplayName();
+				if ($filter['filterType'] == 'matches') {
+					if (strcasecmp($userDisplayName, $filter['filterValue']) == 0) {
+						$matchingUserIds[] = $user->id;
+					}
+				} elseif ($filter['filterType'] == 'contains') {
+					if (stripos($userDisplayName, $filter['filterValue']) !== false) {
+						$matchingUserIds[] = $user->id;
+					}
+				} elseif ($filter['filterType'] == 'startsWith') {
+					if (stripos($userDisplayName, $filter['filterValue']) === 0) {
+						$matchingUserIds[] = $user->id;
+					}
+				}
+			}
+			if (empty($matchingUserIds)) {
+				$object->whereAdd('addedBy = -1');
+			} else {
+				$object->whereAdd('addedBy IN (' . implode(',', $matchingUserIds) . ')');
+			}
+		} else {
+			parent::applyFilter($object, $fieldName, $filter);
+		}
+	}
+
 }
